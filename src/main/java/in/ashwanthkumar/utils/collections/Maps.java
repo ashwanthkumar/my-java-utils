f2bce74d7f9a0d9d5ff6651ca94a491b8f9ed545
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-07-17 22:27:04 +0530

    Adding Maps#empty

--- src/main/java/in/ashwanthkumar/utils/collections/Maps.java
+++ src/main/java/in/ashwanthkumar/utils/collections/Maps.java
@@ -1,9 +1,14 @@
 package in.ashwanthkumar.utils.collections;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Maps {
+    public static <K, V> Map<K, V> empty() {
+        return Collections.emptyMap();
+    }
+
     public static <K, V> Map<K, V> of(K key, V value) {
         HashMap<K, V> map = new HashMap<K, V>();
         map.put(key, value);
@@ -43,16 +48,19 @@ public class Maps {
     public static class MapBuilder<K, V> {
         private Map<K, V> internalMap = new HashMap<K, V>();
 
-        private MapBuilder() {}
+        private MapBuilder() {
+        }
 
         public MapBuilder<K, V> put(K key, V value) {
             internalMap.put(key, value);
             return this;
         }
+
         public Map<K, V> value() {
             return internalMap;
         }
     }
+
     public static <K, V> MapBuilder<K, V> builder() {
         return new MapBuilder<K, V>();
     }
